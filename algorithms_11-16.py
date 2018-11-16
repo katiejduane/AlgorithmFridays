af83361a3e6aea23b8beb4e22c08b2d6efe34041
Katie Duane <katiejduane@gmail.com>
2018-11-16 10:20:59 -0500

    first two algorithms

---
+++ algorithms_11-16.py
@@ -0,0 +1,25 @@
+# 1. Total sum of all numbers divisible by 3 or 5
+# We need to make a counter, use % for 5 or 3, and sum all the numbers in another variable as it counts
+# total_sum = 0
+# # counter = 0 (use for while?)
+# for i in range(0, 1000):
+#     if i % 3 == 0 or i % 5 == 0:
+#         total_sum += i
+
+# print total_sum
+
+
+# fib_seq = [1, 2]
+# sum = 0
+# running_total = 2
+# for i in fib_seq:
+#     if i < 4000000:
+#         sum = fib_seq[-2] + fib_seq[-1]
+#         fib_seq.append(sum)
+#         if sum % 2 == 0:
+#             running_total += sum
+#             print running_total
+
+# print running_total
+# print fib_seq
+
