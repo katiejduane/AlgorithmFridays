413427b1db91a28e2da971908a130cf6fb499b08
Katie Duane <katiejduane@gmail.com>
2019-06-01 21:17:01 -0400

    binary search

--- AlgorithmsAndDataStructures_Udemy/3-searchingAlgorithms/binarySearch.js
+++ AlgorithmsAndDataStructures_Udemy/3-searchingAlgorithms/binarySearch.js
@@ -23,11 +23,14 @@ function binarySearch(arr, elem) {
     var end = arr.length - 1;
     var middle = Math.floor((start + end) / 2);
     while (arr[middle] !== elem && start <= end) {
-        if (elem < arr[middle]) end = middle - 1;
-        else start = middle + 1;
+        if (elem < arr[middle]) {
+            end = middle - 1;
+        } else {
+            start = middle + 1;
+        }
         middle = Math.floor((start + end) / 2);
     }
     return arr[middle] === elem ? middle : -1;
 }
 
-binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103)
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 13))
