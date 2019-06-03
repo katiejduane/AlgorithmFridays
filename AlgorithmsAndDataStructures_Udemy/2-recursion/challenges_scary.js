0c2bcd028156af87b4fecdf6f4c508fd236b4b27
Katie Duane <katiejduane@gmail.com>
2019-06-03 18:10:33 -0400

    werking on recursion

--- AlgorithmsAndDataStructures_Udemy/2-recursion/challenges_scary.js
+++ AlgorithmsAndDataStructures_Udemy/2-recursion/challenges_scary.js
@@ -15,23 +15,32 @@ console.log(reverse(stringToRev));
 // isPalindrome('tacocat') // true
 // isPalindrome('amanaplanacanalpanama') // true
 // isPalindrome('amanaplanacanalpandemonium') // false
-function isPalindrome(str){
-  // add whatever parameters you deem necessary - good luck!
+
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1))
+  return false;
 }
 
 
 //write a function which accepts an array and a callback. the function returns true if a single value
 //in the array returns true when passed to the callback, otherwise it returns false.
 // SAMPLE INPUT / OUTPUT
-// const isOdd = val => val % 2 !== 0;
-
 // someRecursive([1,2,3,4], isOdd) // true
 // someRecursive([4,6,8,9], isOdd) // true
 // someRecursive([4,6,8], isOdd) // false
 // someRecursive([4,6,8], val => val > 10); // false
-function someRecursive(){
-  // add whatever parameters you deem necessary - good luck!
+
+const isOdd = val => val % 2 !== 0;
+
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
 }
+
+console.log(someRecursive([1,2,3,4], isOdd))
 
 
 //write a recursive function which accepts an array of arrays and returns a 
@@ -40,7 +49,16 @@ function someRecursive(){
 // flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
 // flatten([[1],[2],[3]]) // [1,2,3]
 // flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3]
-function flatten(){
-  // add whatever parameters you deem necessary - good luck!
+
+function flatten(oldArr) {
+  var newArr = []
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]))
+    } else {
+      newArr.push(oldArr[i])
+    }
+  }
+  return newArr;
 }
 
